7e3f1de99839b4363f7b5c1cd732d1e4fd83952f
Nathan Farley <nathanfarley@Nathans-MacBook-Pro.local>
2022-04-12 08:47:35 -0500

    Updates port and vercel.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const PORT = 3001
+const PORT = 5000
 const { createUser, login, logout, deposit, withdraw, transfer, getUserBalance, getUsers } = require('./db')
 
 app.use(cors())
